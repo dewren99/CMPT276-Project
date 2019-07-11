2a8e4d3dc4c5c8b7f2294e5c2bf1303f90cf044b
nadia <nadiahazar62@gmail.com>
2019-07-11 12:21:15 -0700

    profile city

--- EasyCleaners/app/controllers/profiles_controller.rb
+++ EasyCleaners/app/controllers/profiles_controller.rb
@@ -28,7 +28,7 @@ class ProfilesController < ApplicationController
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
+        format.html { redirect_to @profile, notice: 'Proooofile was successfully created.' }
         format.json { render :show, status: :created, location: @profile }
       else
         format.html { render :new }
